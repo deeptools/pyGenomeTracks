16a5b17f351b0c64d96fcf95656921f521d7dd2d
Fidel Ramirez <fidel.ramirez@gmail.com>
2018-03-06 21:58:40 +0100

    fix typo

--- pygenometracks/tests/test_make_tracks.py
+++ pygenometracks/tests/test_make_tracks.py
@@ -16,10 +16,10 @@ def test_make_tracks():
 
     pygenometracks.makeTracksFile.main(args)
 
-    if filecmp.cmp(outfile.name, ROOT + '/master_tracks_tmp.ini') is False:
+    if filecmp.cmp(outfile.name, ROOT + '/master_tracks.ini') is False:
         import difflib
         diff = difflib.unified_diff(open(outfile.name).readlines(),
-                                    open(ROOT + '/master_tracks_tmp.ini').readlines(), lineterm='')
+                                    open(ROOT + '/master_tracks.ini').readlines(), lineterm='')
         print(''.join(list(diff)))
 
     assert(filecmp.cmp(outfile.name, ROOT + '/master_tracks.ini') is True)
