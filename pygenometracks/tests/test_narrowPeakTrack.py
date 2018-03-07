36c1ec50e32aa4022a60edec8138b3a1bcc98cfb
Fidel Ramirez <fidel.ramirez@gmail.com>
2018-03-07 23:48:46 +0100

    added narrowPeak track class to plot peaks based on macs2 output

---
+++ pygenometracks/tests/test_narrowPeakTrack.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import matplotlib as mpl
+mpl.use('agg')
+from matplotlib.testing.compare import compare_images
+from tempfile import NamedTemporaryFile
+import os.path
+import pygenometracks.plotTracks
+
+ROOT = os.path.dirname(os.path.abspath(__file__)) + "/test_data/"
+
+tracks = """
+[narrow]
+file = test.narrowPeak
+height = 4
+max_value = 40
+title = max_value=40
+
+[narrow 2]
+file = test.narrowPeak
+height = 4
+show labels = no
+show data range =  no
+color = #00FF0080
+use summit = no
+title = show labels=no; show data range=no; use summit=no;color=#00FF0080
+[spacer]
+
+[narrow 3]
+file = test.narrowPeak
+height = 4
+show labels = no
+color = #0000FF80
+use summit = no
+width adjust = 4
+title = show labels=no;width adjust=3
+
+[spacer]
+
+[narrow 4]
+file = test.narrowPeak
+height = 3
+type = box
+color = blue
+title = type=box;color=blue;
+
+[x-axis]
+"""
+
+with open(ROOT + "narrow_peak.ini", 'w') as fh:
+    fh.write(tracks)
+
+tolerance = 13  # default matplotlib pixed difference tolerance
+
+
+def test_narrow_track():
+    region = "X:2760000-2800000"
+    outfile = NamedTemporaryFile(suffix='.png', prefix='narrowTrack_test_', delete=False)
+    args = "--tracks {root}/narrow_peak.ini --region {region} --trackLabelFraction 0.2 " \
+           "--dpi 130 --outFileName  {outfile}".format(root=ROOT, outfile=outfile.name, region=region).split()
+    pygenometracks.plotTracks.main(args)
+    print("saving test to {}".format(outfile.name))
+    res = compare_images(ROOT + '/master_narrowPeak.png', outfile.name, tolerance)
+    assert res is None, res
+
+    os.remove(outfile.name)
--- pygenometracks/tracksClass.py
+++ pygenometracks/tracksClass.py
@@ -2064,6 +2064,140 @@ file_type = {}
                            verticalalignment='center', transform=self.label_ax.transAxes)
 
 
+class NarrowPeakTrack(BedGraphTrack):
+    SUPPORTED_ENDINGS = ['.narrowPeak']
+    TRACK_TYPE = 'narrow_peal'
+    OPTIONS_TXT = GenomeTrack.OPTIONS_TXT + """
+#max_value = 0.70
+show data range = yes
+show labels = yes
+# the narrowPeak format provides the information of the
+# peak summit. By default this information is used
+# although some peaks may look crooked.
+use summit = yes
+# type of plot: either box or peak
+# box will plot a rectangle of the peak width
+# peak will plot the shape of the peak, whose height is the
+# narrowPeak file signal value (usually peak coverage)
+type = peak
+# if the peaks look too thin, the can be adjusted
+width adjust = 1.5
+file_type = {}
+    """.format(TRACK_TYPE)
+
+    def __init__(self, properties_dict):
+        self.properties = properties_dict
+        if 'color' not in self.properties:
+            self.properties['color'] = '#FF000080'  # red, alpha=0.55
+        if 'show data range' not in self.properties:
+            self.properties['show data range'] = 'yes'
+        if 'show labels' not in self.properties:
+            self.properties['show labels'] = 'yes'
+        if 'use summits' not in self.properties:
+            self.properties['use summits'] = 'yes'
+        if 'width adjust' not in self.properties:
+            self.properties['width adjust'] = 1.5
+        else:
+            self.properties['width adjust'] = float(self.properties['width adjust'])
+        if 'type' not in self.properties:
+            self.properties['type'] = 'peak'
+
+        self.interval_tree, _, _ = file_to_intervaltree(self.properties['file'])
+
+    def peak_plot(self, start, end, height, center=None, width_adjust=1.5):
+        # uses bezier curves to plot a shape that
+        # looks like a peak
+        from matplotlib.path import Path
+        import matplotlib.patches as patches
+        peak_width = float(end - start)
+        if center is None:
+            center = peak_width / 2 + start
+        if width_adjust != 1:
+            start -= width_adjust * peak_width / 2
+            end += width_adjust * peak_width / 2
+            peak_width *= width_adjust
+
+        path_data = [
+            (Path.MOVETO, (start, 0)),
+            (Path.CURVE4, (start + peak_width / 2, 0)),
+            (Path.CURVE4, (start + peak_width * 0.4, height)),
+            (Path.CURVE4, (center, height)),
+            (Path.CURVE4, (end - peak_width * 0.4, height)),
+            (Path.CURVE4, (end - peak_width / 2, 0)),
+            (Path.CURVE4, (end, 0))]
+
+        codes, verts = zip(*path_data)
+        path = Path(verts, codes)
+        patch = patches.PathPatch(path, facecolor=self.properties['color'])
+        self.ax.add_patch(patch)
+
+    def plot(self, ax, label_ax, chrom_region, start_region, end_region):
+        self.ax = ax
+
+        chrom_region = check_chrom_str_bytes(self.interval_tree, chrom_region)
+        if chrom_region not in list(self.interval_tree):
+            chrom_region = change_chrom_names(chrom_region)
+            chrom_region = check_chrom_str_bytes(self.interval_tree, chrom_region)
+        max_signal = -1
+        for peak in sorted(self.interval_tree[chrom_region][start_region - 10000:end_region + 10000]):
+            name, score, strand, signal_value, p_value, q_value, summit = peak.data
+            signal_value = float(signal_value)
+            p_value = float(p_value)
+            q_value = float(q_value)
+            summit = int(summit)
+            if summit > 0:
+                summit = peak.begin + summit
+            else:
+                summit = None
+
+            if self.properties['type'] == 'box':
+                self.ax.add_patch(Rectangle((peak.begin, 0), peak.end - peak.begin,
+                                            100, edgecolor='black',
+                                            facecolor=self.properties['color']))
+                max_signal = 110
+            else:
+                if signal_value > max_signal:
+                    max_signal = signal_value
+                self.peak_plot(peak.begin, peak.end, signal_value, center=summit,
+                               width_adjust=self.properties['width adjust'])
+
+            x_pos = peak.begin + float(peak.end - peak.begin) / 2
+            y_pos = 0 - max_signal * 0.05
+            if self.properties['show labels'] != 'no':
+                self.ax.text(x_pos, y_pos, "{}\np-val:{:.1f}\nq-val:{:.1f}".format(name, p_value, q_value),
+                             horizontalalignment='center', size='smaller',
+                             verticalalignment='top')
+
+        if 'max_value' not in self.properties or self.properties['max_value'] == 'auto':
+            self.properties['max_value'] = max_signal
+
+        ymax = self.properties['max_value']
+        if self.properties['show labels'] != 'no':
+            if self.properties['type'] == 'box':
+                ymin = ymax * -3
+            else:
+                ymin = ymax * -0.8
+        else:
+            ymin = 0
+        self.ax.set_ylim(ymin, ymax)
+        if self.properties['show data range'] != 'no' and self.properties['type'] != 'box':
+            if float(ymax) % 1 == 0:
+                ymax_print = int(ymax)
+            else:
+                ymax_print = "{:.1f}".format(ymax)
+            ymin_print = 0
+            small_x = 0.01 * (end_region - start_region)
+            # by default show the data range
+            self.ax.text(start_region - small_x, ymax - ymax * 0.01,
+                         "[{}-{}]".format(ymin_print, ymax_print),
+                         horizontalalignment='left',
+                         verticalalignment='top')
+
+        label_ax.text(0.15, 0.5, self.properties['title'],
+                      horizontalalignment='left', size='large',
+                      verticalalignment='center')
+
+
 def change_chrom_names(chrom):
     """
     Changes UCSC chromosome names to ensembl chromosome names
