f788e2948641492b1e8ff8476cb97f7c314ec5d0
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2018-07-02 12:27:02 -0700

    Fixing scale labels and pep

--- pygenometracks/tracksClass.py
+++ pygenometracks/tracksClass.py
@@ -455,15 +455,16 @@ class XAxisTrack(GenomeTrack):
 
     def plot(self, ax, chrom_region, region_start, region_end):
         ticks = ax.get_xticks()
-        if ticks[-1] - ticks[1] <= 1e5:
+        if ticks[-1] - ticks[1] <= 1e3:
+            labels = ["{:,.0f}".format((x))
+                      for x in ticks]
+            labels[-2] += " b"
+
+        elif ticks[-1] - ticks[1] <= 4e5:
             labels = ["{:,.0f}".format((x / 1e3))
                       for x in ticks]
             labels[-2] += " Kb"
 
-        elif 1e5 < ticks[-1] - ticks[1] < 4e6:
-            labels = ["{:,.0f}".format((x / 1e3))
-                      for x in ticks]
-            labels[-2] += " Kb"
         else:
             labels = ["{:,.1f} ".format((x / 1e6))
                       for x in ticks]
