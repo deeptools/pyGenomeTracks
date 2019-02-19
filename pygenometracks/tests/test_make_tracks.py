a70e6f8441142699d9a408a99e2ed7198da2bab6
Leily Rabbani <rabbani@pc390.ie-freiburg.mpg.de>
2019-02-19 17:18:57 +0100

    changed test path in test_make_track.py

--- pygenometracks/tests/test_make_tracks.py
+++ pygenometracks/tests/test_make_tracks.py
@@ -2,7 +2,6 @@ from tempfile import NamedTemporaryFile
 import os.path
 import filecmp
 import pygenometracks.makeTracksFile
-import difflib
 
 ROOT = os.path.dirname(os.path.abspath(__file__)) + "/test_data/"
 # the relative path is needed to compare the two .ini files correctly. Otherwise
@@ -11,18 +10,18 @@ relative_path = os.path.relpath(ROOT)
 
 
 def test_make_tracks():
-    outfile = NamedTemporaryFile(suffix='.ini', prefix='pyGenomeTracks_test_', delete=False)
+    outfile = NamedTemporaryFile(suffix='.ini', prefix='pyGenomeTracks/test/', delete=False)
     args = "--trackFiles {0}/Li_et_al_2015.h5 {0}/bigwig_chrx_2e6_5e6.bw {0}/tad_classification.bed " \
            "{0}/epilog.qcat.bgz " \
            "--out {1}".format(relative_path, outfile.name).split()
     print("using args: {}".format(" ".join(args)))
     pygenometracks.makeTracksFile.main(args)
 
-    if filecmp.cmp(outfile.name, ROOT + '/master_tracks.ini') is False:
+    if filecmp.cmp(outfile.name, ROOT + 'master_tracks.ini') is False:
+        import difflib
         diff = difflib.unified_diff(open(outfile.name).readlines(),
-                                    open(ROOT + '/master_tracks.ini').readlines(), lineterm='')
+                                    open(ROOT + 'master_tracks.ini').readlines(), lineterm='')
         print(''.join(list(diff)))
+    assert(filecmp.cmp(outfile.name, ROOT + 'master_tracks.ini') is True)
 
-    assert(filecmp.cmp(outfile.name, ROOT + '/master_tracks.ini') is True)
-
-    os.remove(outfile.name)
+os.remove(outfile.name)
