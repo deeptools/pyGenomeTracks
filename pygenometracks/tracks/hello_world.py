5f82ddd7931244e613184d7cc0427bb218e35e7a
ykohki <43834721+ykohki@users.noreply.github.com>
2019-05-04 18:26:43 +0900

    hello_world.pyをpygenometracks/tracksに追加。

---
+++ pygenometracks/tracks/hello_world.py
@@ -0,0 +1,23 @@
+class TextTrack(GenomeTrack):
+    SUPPORTED_ENDINGS = ['.txt']  # this is used by make_tracks_file to guess the type of track based on file name
+    TRACK_TYPE = 'text'
+    OPTIONS_TXT = """
+height = 3
+title =
+text =
+# x position of text in the plot (in bp)
+x position =
+"""
+    def plot(self, ax, chrom, region_start, region_end):
+        """
+        This example simply plots the given title at a fixed
+        location in the axis. The chrom, region_start and region_end
+        variables are not used.
+        Args:
+            ax: matplotlib axis to plot
+            chrom_region: chromosome name
+            start_region: start coordinate of genomic position
+            end_region: end coordinate
+        """
+        # print text at position x = self.properties['x position'] and y = 0.5 (center of the plot)
+        ax.text(float(self.properties['x position']), 0.5, self.properties['text'])
