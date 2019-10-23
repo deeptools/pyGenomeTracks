79f7077410a1c91db664af937e68c7d688d06239
Lucille Delisle <lucille.delisle@epfl.ch>
2019-10-23 15:06:28 +0200

    added an appropriate test for use middle

---
+++ pygenometracks/tests/test_bedGraphTrack.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import matplotlib as mpl
+mpl.use('agg')
+from matplotlib.testing.compare import compare_images
+from tempfile import NamedTemporaryFile
+import os.path
+import pygenometracks.plotTracks
+
+ROOT = os.path.dirname(os.path.abspath(__file__)) + "/test_data/"
+
+browser_tracks = """
+[x-axis]
+where = top
+
+[spacer]
+height = 0.05
+
+[test bedgraph]
+file = GSM3182416_E12DHL_WT_Hoxd11vp.bedgraph.gz
+color = blue
+height = 5
+title = bedgraph
+max_value = 10
+
+[test bedgraph use middle]
+file = GSM3182416_E12DHL_WT_Hoxd11vp.bedgraph.gz
+color = blue
+height = 5
+title = bedgraph with use middle = yes
+max_value = 10
+use middle = yes
+
+[genes]
+file = HoxD_cluster_regulatory_regions_mm10.bed
+height = 3
+title = "HoxD genes and regulatory regions"
+
+"""
+with open(ROOT + "bedgraph_useMid.ini", 'w') as fh:
+    fh.write(browser_tracks)
+
+
+tolerance = 13  # default matplotlib pixed difference tolerance
+
+
+def test_plot_tracks():
+
+    outfile = NamedTemporaryFile(suffix='.png', prefix='pyGenomeTracks_test_', delete=False)
+    args = "--tracks {0}/bedgraph_useMid.ini --region chr2:73,800,000-75,744,000 --trackLabelFraction 0.2 --width 38 " \
+           "--dpi 130 --outFileName  {1}".format(ROOT, outfile.name).split()
+    pygenometracks.plotTracks.main(args)
+    print("saving test to {}".format(outfile.name))
+    res = compare_images(ROOT + '/master_bedgraph_useMid.png', outfile.name, tolerance)
+    assert res is None, res
+
+    os.remove(outfile.name)
