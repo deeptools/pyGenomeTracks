efbbcad68e2a35b9eef3ef900a350533bb64af97
Lucille Delisle <lucille.delisle@epfl.ch>
2019-10-25 11:03:27 +0200

    added a test for rasterize

--- pygenometracks/tests/test_bedGraphTrack.py
+++ pygenometracks/tests/test_bedGraphTrack.py
@@ -19,6 +19,14 @@ height = 0.05
 file = GSM3182416_E12DHL_WT_Hoxd11vp.bedgraph.gz
 color = blue
 height = 5
+title = bedgraph rasterize = yes
+rasterize = yes
+max_value = 10
+
+[test bedgraph]
+file = GSM3182416_E12DHL_WT_Hoxd11vp.bedgraph.gz
+color = blue
+height = 5
 title = bedgraph
 max_value = 10
 
@@ -43,7 +51,7 @@ with open(ROOT + "bedgraph_useMid.ini", 'w') as fh:
 tolerance = 13  # default matplotlib pixed difference tolerance
 
 
-def test_plot_tracks():
+def test_plot_bedgraph_tracks():
 
     outfile = NamedTemporaryFile(suffix='.png', prefix='pyGenomeTracks_test_', delete=False)
     args = "--tracks {0}/bedgraph_useMid.ini --region chr2:73,800,000-75,744,000 --trackLabelFraction 0.2 --width 38 " \
@@ -51,6 +59,18 @@ def test_plot_tracks():
     pygenometracks.plotTracks.main(args)
     print("saving test to {}".format(outfile.name))
     res = compare_images(ROOT + '/master_bedgraph_useMid.png', outfile.name, tolerance)
+    assert res is None, res
+
+    os.remove(outfile.name)
+
+def test_plot_bedgraph_tracks_rasterize():
+
+    outfile = NamedTemporaryFile(suffix='.pdf', prefix='pyGenomeTracks_test_', delete=False)
+    args = "--tracks {0}/bedgraph_useMid.ini --region chr2:73,800,000-75,744,000 --trackLabelFraction 0.2 --width 38 " \
+           "--dpi 130 --outFileName  {1}".format(ROOT, outfile.name).split()
+    pygenometracks.plotTracks.main(args)
+    print("saving test to {}".format(outfile.name))
+    res = compare_images(ROOT + '/master_bedgraph_useMid.pdf', outfile.name, tolerance)
     assert res is None, res
 
     os.remove(outfile.name)
