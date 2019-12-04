561825c9b3eca15360be815004a67064f934d609
Lucille Delisle <lucille.delisle@epfl.ch>
2019-12-04 22:54:17 +0100

    split tests and added more tests

--- pygenometracks/tests/test_bed_and_gtf_tracks.py
+++ pygenometracks/tests/test_bed_and_gtf_tracks.py
@@ -50,22 +50,6 @@ gene_rows = 10
 [spacer]
 height = 1
 
-[test bed6 arrowhead_included]
-file = dm3_genes.bed6.gz
-height = 7
-title = bed6 border_color = black; gene_rows=10; fontsize=8; color=Reds
-        (when a color map is used for the color (e.g. coolwarm, Reds) the bed
-        score column mapped to a color); arrowhead_included = true
-fontsize = 7
-file_type = bed
-color = Reds
-border_color = black
-arrowhead_included = true
-gene_rows = 10
-
-[spacer]
-height = 1
-
 [test bed4]
 file = dm3_genes.bed4.gz
 height = 10
@@ -124,6 +108,101 @@ with open(os.path.join(ROOT, "bed_and_gtf_tracks.ini"), 'w') as fh:
 browser_tracks = """
 [x-axis]
 where = top
+title = where =top
+
+[spacer]
+height = 0.05
+
+[genes 2]
+file = dm3_genes.bed.gz
+height = 3
+title = genes (bed12) style = UCSC; fontsize = 10
+style = UCSC
+fontsize = 10
+
+[genes 2bis]
+file = dm3_genes.bed.gz
+height = 3
+title = genes (bed12) style = UCSC; arrow_interval=10; fontsize = 10
+style = UCSC
+arrow_interval = 10
+fontsize = 10
+
+[spacer]
+height = 1
+
+[test bed6]
+file = dm3_genes.bed6.gz
+height = 3
+title = bed6 border_color = black; fontsize=8; color=red
+fontsize = 8
+file_type = bed
+color = red
+border_color = black
+
+[spacer]
+height = 1
+
+[test bed6 arrowhead_included]
+file = dm3_genes.bed6.gz
+height = 3
+title = bed6 border_color = black; fontsize=8; color=red; arrowhead_included = true
+fontsize = 8
+file_type = bed
+arrowhead_included = true
+
+[spacer]
+height = 1
+
+[test bed4]
+file = dm3_genes.bed4.gz
+height = 3
+title = bed4 fontsize = 10; line_width = 1.5
+fontsize = 10
+file_type = bed
+interval_height = 200
+line_width = 1.5
+
+[spacer]
+height = 1
+
+[test bed]
+file = dm3_subset_BDGP5.78_asbed_sorted.bed.gz
+height = 8
+title = gtf from ensembl in bed12
+fontsize = 12
+file_type = bed
+
+[spacer]
+height = 1
+
+[test bed]
+file = dm3_subset_BDGP5.78_asbed_sorted.bed.gz
+height = 8
+title = gtf from ensembl in bed12; arrowhead_included = true
+fontsize = 12
+file_type = bed
+arrowhead_included = true
+
+[spacer]
+height = 1
+
+[x-axis]
+fontsize = 30
+title = fontsize = 30
+
+[vlines]
+type = vlines
+file = dm3_genes.bed4.gz
+
+"""
+with open(os.path.join(ROOT, "bed_arrow_tracks.ini"), 'w') as fh:
+    fh.write(browser_tracks)
+
+
+browser_tracks = """
+[x-axis]
+where = top
 
 [spacer]
 height = 0.05
@@ -165,6 +244,40 @@ def test_plot_tracks_bed_and_gtf():
                      outfile.name).split()
     pygenometracks.plotTracks.main(args)
     res = compare_images(os.path.join(ROOT, 'master_bed_and_gtf.png'),
+                         outfile.name, tolerance)
+    assert res is None, res
+
+    os.remove(outfile.name)
+
+
+def test_plot_tracks_bed_arrow():
+
+    outfile = NamedTemporaryFile(suffix='.png', prefix='pyGenomeTracks_test_',
+                                 delete=False)
+    args = "--tracks {0} --region X:3120000-3150000 "\
+           "--trackLabelFraction 0.2 --width 38 --dpi 130 "\
+           "--outFileName {1}" \
+           "".format(os.path.join(ROOT, 'bed_arrow_tracks.ini'),
+                     outfile.name).split()
+    pygenometracks.plotTracks.main(args)
+    res = compare_images(os.path.join(ROOT, 'master_bed_arrow.png'),
+                         outfile.name, tolerance)
+    assert res is None, res
+
+    os.remove(outfile.name)
+
+
+def test_plot_tracks_bed_arrow_zoom():
+
+    outfile = NamedTemporaryFile(suffix='.png', prefix='pyGenomeTracks_test_',
+                                 delete=False)
+    args = "--tracks {0} --region X:3130000-3140000 "\
+           "--trackLabelFraction 0.2 --width 38 --dpi 130 "\
+           "--outFileName {1}" \
+           "".format(os.path.join(ROOT, 'bed_arrow_tracks.ini'),
+                     outfile.name).split()
+    pygenometracks.plotTracks.main(args)
+    res = compare_images(os.path.join(ROOT, 'master_bed_arrow_zoom.png'),
                          outfile.name, tolerance)
     assert res is None, res
 
