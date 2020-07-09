f5b7322838adcd788befeeadd0ec3858b794ee58
Lucille Delisle <lucille.delisle@epfl.ch>
2020-07-09 17:40:40 +0200

    update bash script

--- evaluateCoverage.sh
+++ evaluateCoverage.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 conda create -n pgt_dev --file requirements.txt -y
 conda activate pgt_dev
-conda install -y pytest nose ghostscript pathlib coverage
+conda install -y pytest nose ghostscript pathlib coverage coverage-badge
 python setup.py install
 coverage run -m py.test
 coverage html
-pip install coverage-badge
-coverage-badge -o docs/coverage.svg
+coverage-badge -f -o docs/coverage.svg
