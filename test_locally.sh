3cc6f670ab7340266ee2981e26c4f595448ed319
Lucille Delisle <lucille.delisle@epfl.ch>
2020-10-29 15:08:22 +0100

    add python3.9 in local tests

--- test_locally.sh
+++ test_locally.sh
@@ -12,3 +12,12 @@ for TRAVIS_PYTHON_VERSION in 3.6 3.7 3.8; do
     coverage-badge -f -o docs/coverage.svg
     conda deactivate
 done
+
+TRAVIS_PYTHON_VERSION=3.9
+conda create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda -c conda-forge python=$TRAVIS_PYTHON_VERSION
+conda activate pgt_test_${TRAVIS_PYTHON_VERSION}
+conda install --yes -c conda-forge -c bioconda numpy bedtools h5py pytest ghostscript
+pip install -r requirements.txt
+python setup.py install
+py.test pygenometracks --doctest-modules
+conda deactivate
