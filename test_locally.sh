8f3b2ad2307fea4fcd21cf4d91b4dddc6977a3a8
Lucille Delisle <lucille.delisle@epfl.ch>
2020-10-29 07:53:35 +0100

    test on python 3.8

--- test_locally.sh
+++ test_locally.sh
@@ -2,11 +2,9 @@
 
 source $(dirname $(dirname $(which conda)))/etc/profile.d/conda.sh
 
-for TRAVIS_PYTHON_VERSION in 3.6 3.7; do
+for TRAVIS_PYTHON_VERSION in 3.6 3.7 3.8; do
     conda create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda -c conda-forge python=$TRAVIS_PYTHON_VERSION --file requirements.txt
     conda activate pgt_test_${TRAVIS_PYTHON_VERSION}
-    # Keeping libopenblas=0.3.10 gives me segmentation fault
-    conda install --yes -c conda-forge -c bioconda libopenblas=0.3.9
     conda install --yes -c conda-forge -c bioconda pytest ghostscript coverage coverage-badge
     python setup.py install
     coverage run -m py.test
@@ -14,12 +12,3 @@ for TRAVIS_PYTHON_VERSION in 3.6 3.7; do
     coverage-badge -f -o docs/coverage.svg
     conda deactivate
 done
-
-TRAVIS_PYTHON_VERSION=3.8
-conda create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda -c conda-forge python=$TRAVIS_PYTHON_VERSION
-conda activate pgt_test_${TRAVIS_PYTHON_VERSION}
-conda install --yes -c conda-forge -c bioconda bedtools pytest ghostscript
-pip install -r requirements.txt
-python setup.py install
-py.test pygenometracks --doctest-modules
-conda deactivate
