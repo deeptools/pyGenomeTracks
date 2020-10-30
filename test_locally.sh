880d6721ee1de890694897044f55d49c0c50e371
Lucille Delisle <lucille.delisle@epfl.ch>
2020-10-30 10:39:20 +0100

    use a different requirement file for travis

--- test_locally.sh
+++ test_locally.sh
@@ -3,7 +3,7 @@
 source $(dirname $(dirname $(which conda)))/etc/profile.d/conda.sh
 
 for TRAVIS_PYTHON_VERSION in 3.6 3.7 3.8; do
-    conda create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda -c conda-forge python=$TRAVIS_PYTHON_VERSION --file requirements.txt
+    conda create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda -c conda-forge python=$TRAVIS_PYTHON_VERSION --file requirements_travis.txt
     conda activate pgt_test_${TRAVIS_PYTHON_VERSION}
     conda install --yes -c conda-forge -c bioconda pytest ghostscript coverage coverage-badge
     python setup.py install
@@ -17,7 +17,7 @@ TRAVIS_PYTHON_VERSION=3.9
 conda create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda -c conda-forge python=$TRAVIS_PYTHON_VERSION
 conda activate pgt_test_${TRAVIS_PYTHON_VERSION}
 conda install --yes -c conda-forge -c bioconda numpy bedtools h5py pytest ghostscript
-pip install -r requirements.txt
+pip install -r requirements_travis.txt
 python setup.py install
 py.test pygenometracks --doctest-modules
 conda deactivate
