22287638c782c7cd65988fa8cd6d273c6ddd9ad5
Lucille Delisle <lucille.delisle@epfl.ch>
2020-11-03 21:59:29 +0100

    catch NA in second bedgraph

--- pygenometracks/tracks/BedGraphTrack.py
+++ pygenometracks/tracks/BedGraphTrack.py
@@ -348,7 +348,15 @@ file_type = {TRACK_TYPE}
                                                      inttree_var='self.interval_tree2')
             if pos_list2 == []:
                 return
-            score_list2 = [float(x[0]) for x in score_list2]
+            try:
+                score_list2 = [float(x[0]) for x in score_list2]            
+            except ValueError as ve:
+                if "could not convert string to float: 'NA'" in str(ve):
+                    self.log.warning("*Warning*\nNA were found in the bedgraph"
+                                    " will be replaced by nan.\n")
+                    score_list2 = [float(x[0]) if x[0] != 'NA' else float('nan') for x in score_list2]
+                else:
+                    raise ve
             score_list2, x_values2 = self.get_values_as_bigwig(score_list2,
                                                                pos_list2,
                                                                chrom_region,
