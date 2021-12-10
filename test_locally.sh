a2343b0c32897f3cc8b9e01064b28f8c4edabd2b
Lucille Delisle <lucille.delisle@epfl.ch>
2021-12-10 21:48:42 +0100

    use a single requirement file.

--- test_locally.sh
+++ test_locally.sh
@@ -5,7 +5,6 @@ source $(dirname $(dirname $(which conda)))/etc/profile.d/conda.sh
 for TRAVIS_PYTHON_VERSION in 3.6 3.7 3.8; do
     conda create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda -c conda-forge python=$TRAVIS_PYTHON_VERSION --file requirements_travis.txt
     conda activate pgt_test_${TRAVIS_PYTHON_VERSION}
-    conda install --yes -c conda-forge -c bioconda pytest ghostscript coverage coverage-badge
     python setup.py install
     coverage run -m py.test
     coverage html
